5116c3d036a8a34d0981aa6e34468e3d1dd4d51e
tatsuya <ryuuma1005@gmail.com>
2019-02-20 05:31:37 +0900

    base

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: :index
+  get 'user/matsuoka' => 'users#matsuoka'
+  get 'user/tatsuya' => 'users#tatsuya'
+  root 'users#index'
 end
