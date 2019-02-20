afb8e0ccac57969b0ca5e2b470fc1c91b5425679
tatsuya <ryuuma1005@gmail.com>
2019-02-20 16:08:55 +0900

    finish simple image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
+	def tatsuya
+		redirect_to users_path
+	end
+
+	def matsuoka
+		redirect_to users_path
+	end
 end
