3340e5e58db8512ffbf3424e8609d6fa2dfce4ac
tatsuya <ryuuma1005@gmail.com>
2019-02-20 17:14:58 +0900

    finish basic application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,16 @@ class UsersController < ApplicationController
 	end
 
 	def tatsuya
+		@user = User.find_by(id: 1)
+		@user.passion += 1
+		@user.save
 		redirect_to users_path
 	end
 
 	def matsuoka
+		@user = User.find_by(id: 2)
+		@user.passion += 1
+		@user.save
 		redirect_to users_path
 	end
 end
